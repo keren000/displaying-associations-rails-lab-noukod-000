9bc26c30b234208b7285d957e74eb9dca371e08a
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-29 15:27:39 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,8 @@
 # It's strongly recommended that you check this file into your version control system.
 
 #
-# ActiveRecord::Schema.define(version: 20160108015408) do
+ ActiveRecord::Schema.define(version: 20160108015408) do
 
-ActiveRecord::Schema.define(version: 1) do
   create_table "artists", force: :cascade do |t|
       t.string   "name"
       t.datetime "created_at", null: false
